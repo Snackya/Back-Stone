630305a23d7b8b0c67c6942c97a17976d5f5a667
Jannik Bikowski <janbiko@gmx.de>
2017-04-23 12:42:04 +0200

    Implemented basic player movement

---
+++ PSMG17/Assets/Player/scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed = 4f;
+
+    private Rigidbody2D playerBody;
+
+    private Vector3 movementInput;
+    private Vector3 movementVelocity;
+
+	// Use this for initialization
+	void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        movementInput = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        movementVelocity = movementInput * moveSpeed;
+	}
+
+    void FixedUpdate()
+    {
+        playerBody.velocity = movementVelocity;
+    }
+}
