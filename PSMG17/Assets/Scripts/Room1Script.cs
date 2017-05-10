bd041546f8bd790dc9b1172ab2818e7522974e01
Jannik Bikowski <janbiko@gmx.de>
2017-05-10 22:29:54 +0200

    Implemented experimental first room
    - Die Platzhaltergrafiken wurden durch eigene ersetzt
    - ein erster Raum wurde gebaut, mit Wiedergeburtsbrunnen im Zentrum
    - wenn beide Spieler den Raum verlassen haben, schließt sich ein Tor

---
+++ PSMG17/Assets/Scripts/Room1Script.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room1Script : MonoBehaviour {
+
+    [SerializeField]
+    private Transform player1;
+    [SerializeField]
+    private Transform player2;
+
+    private BoxCollider2D room;
+    private Bounds roomBounds;
+    private GameObject gate;
+    private bool playersInside = true;
+
+
+	// Use this for initialization
+	void Start () {
+        room = GetComponent<BoxCollider2D>();
+        roomBounds = room.bounds;
+        gate = transform.Find("Gate").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (playersInside)
+        {
+            if (!roomBounds.Contains(player1.position) &&
+            !roomBounds.Contains(player2.position))
+            {
+                gate.SetActive(true);
+                playersInside = false;
+            }
+        }
+	}
+}
