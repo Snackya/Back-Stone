ccfa4a8defaa08cc82f895624dbb8731d0f7b2ee
Jannik Bikowski <janbiko@gmx.de>
2017-06-25 22:51:03 +0200

    Camera can now focus on two seperate rooms

--- PSMG17/Assets/Scripts/CameraTest.cs
+++ PSMG17/Assets/Scripts/CameraTest.cs
@@ -13,12 +13,13 @@ public class CameraTest : MonoBehaviour {
     private List<Transform> rooms;
     private BoxCollider2D curRoomCollider;
     private Bounds curRoomBounds;
+    private Bounds nextRoomBounds;
 
     private Camera gameCamera;
     private float zoomSpeed;
     private float dampTime = 0.5f;
     private Vector3 moveVelocity;
-    private float[] area01CamSizes = new float[] { 5f, 6.5f, 6f, 7.5f };
+    private float[] area01CamSizes = new float[] { 5.5f, 7f, 6.5f, 8f };
     
 
     private void Awake()
@@ -53,18 +54,39 @@ public class CameraTest : MonoBehaviour {
 
     private void SetCameraPosition()
     {
+        Vector3 player1Pos = players[0].position;
+        Vector3 player2Pos = players[1].position;
+
         // iterating through the rooms
         for (int i = 0; i < rooms.Count; i++)
         {
             curRoomCollider = rooms[i].GetComponent<BoxCollider2D>();
             curRoomBounds = curRoomCollider.bounds;
 
+            if (i != rooms.Count - 1) nextRoomBounds = rooms[i + 1].GetComponent<BoxCollider2D>().bounds;
+
             /* checking if the current room contains both players and smoothly sets the camera position
              * to the room if true */
-            if (curRoomBounds.Contains(players[0].position) && curRoomBounds.Contains(players[1].position))
+            if (curRoomBounds.Contains(player1Pos) && curRoomBounds.Contains(player2Pos))
             {
-                transform.position = Vector3.SmoothDamp(transform.position, curRoomBounds.center, ref moveVelocity, dampTime);
-                gameCamera.orthographicSize = Mathf.SmoothDamp(gameCamera.orthographicSize, area01CamSizes[i], ref zoomSpeed, dampTime);
+                transform.position = Vector3.SmoothDamp(transform.position, 
+                    curRoomBounds.center, 
+                    ref moveVelocity, 
+                    dampTime);
+                gameCamera.orthographicSize = Mathf.SmoothDamp(gameCamera.orthographicSize, 
+                    area01CamSizes[i], 
+                    ref zoomSpeed, 
+                    dampTime);
+            }
+            /* if one player is in another room, than the other one, the camera focuses on the center 
+             * of those two rooms */
+            else if (nextRoomBounds.Contains(player1Pos) && curRoomBounds.Contains(player2Pos) || 
+                nextRoomBounds.Contains(player2Pos) && curRoomBounds.Contains(player1Pos))
+            {
+                transform.position = Vector3.SmoothDamp(transform.position,
+                    (curRoomBounds.center + nextRoomBounds.center) / 2,
+                    ref moveVelocity,
+                    dampTime);
             }
         }
     }
