04fcaa88f2a7380593619ad947e47add49592223
Jannik Bikowski <janbiko@gmx.de>
2017-07-03 23:45:46 +0200

    Added particle effect to enemies
    - Wenn Gegner angegriffen werden, wird nun ein Blut-Partikel Effekt
    abgespielt.

---
+++ PSMG17/Assets/Scripts/Enemies/EnemyParticleControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyParticleControl : MonoBehaviour {
+
+    [SerializeField]
+    private Transform bloodSplash;
+
+	// Use this for initialization
+	void Start () {
+        bloodSplash.GetComponent<ParticleSystem>().enableEmission = false;
+	}
+
+    private void OnTriggerEnter2D()
+    {
+        bloodSplash.GetComponent<ParticleSystem>().enableEmission = true;
+        StartCoroutine(StopBloodSplash());
+    }
+
+    private IEnumerator StopBloodSplash()
+    {
+        yield return new WaitForSeconds(.4f);
+        bloodSplash.GetComponent<ParticleSystem>().enableEmission = false;
+    }
+}
