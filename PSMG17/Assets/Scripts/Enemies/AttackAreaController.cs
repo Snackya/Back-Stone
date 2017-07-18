39dd094862053338b246f12d62cfc0369f305b48
Snackya <thomas.fischeer@gmail.com>
2017-07-18 07:25:58 +0200

    implemented BlackKnight, adjusted player scale

---
+++ PSMG17/Assets/Scripts/Enemies/AttackAreaController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackAreaController : MonoBehaviour {
+    private GameObject parent;
+    private Animator animator;
+	// Use this for initialization
+	void Start () {
+        parent = transform.parent.gameObject;
+        animator = parent.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other);
+    }
+}
