25684e7f1231858595a224e84acbfe544a665f68
Jannik Bikowski <janbiko@gmx.de>
2017-08-21 14:55:09 +0200

    Added teleporters to Riddleguy room

---
+++ PSMG17/Assets/Scripts/TeleporterScript.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterScript : MonoBehaviour {
+
+    [SerializeField]
+    private Transform teleporterSibling;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            StartCoroutine(Teleport(collision));
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            StopAllCoroutines();
+        }
+    }
+
+    private IEnumerator Teleport(Collider2D collision)
+    {
+        yield return new WaitForSeconds(2f);
+        collision.transform.GetComponent<Rigidbody2D>().MovePosition(teleporterSibling.position);
+    }
+}
