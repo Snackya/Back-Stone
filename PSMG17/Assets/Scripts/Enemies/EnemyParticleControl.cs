1459d3eb81e1954dca25f3e0d6123f9587bb716f
Jannik Bikowski <janbiko@gmx.de>
2017-08-22 22:27:40 +0200

    Added healing particle effect to player
    - fixed enemy particle effect bug

--- PSMG17/Assets/Scripts/Enemies/EnemyParticleControl.cs
+++ PSMG17/Assets/Scripts/Enemies/EnemyParticleControl.cs
@@ -8,20 +8,8 @@ public class EnemyParticleControl : MonoBehaviour {
     [SerializeField]
     private Transform bloodSplash;
 
-	// Use this for initialization
-	void Start () {
-        bloodSplash.GetComponent<ParticleSystem>().enableEmission = false;
-	}
-
     private void OnTriggerEnter2D()
     {
-        bloodSplash.GetComponent<ParticleSystem>().enableEmission = true;
-        StartCoroutine(StopBloodSplash());
-    }
-
-    private IEnumerator StopBloodSplash()
-    {
-        yield return new WaitForSeconds(.4f);
-        bloodSplash.GetComponent<ParticleSystem>().enableEmission = false;
+        bloodSplash.GetComponent<ParticleSystem>().Play();
     }
 }
