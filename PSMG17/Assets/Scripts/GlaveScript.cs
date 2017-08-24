2ba4ee9b61d23480c6aad7b468e6cc394d09f5d5
Jannik Bikowski <janbiko@gmx.de>
2017-08-24 12:21:54 +0200

    Implemented glaves room

---
+++ PSMG17/Assets/Scripts/GlaveScript.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlaveScript : MonoBehaviour {
+
+    private float rotationSpeed = 70f;
+    [SerializeField] private float movementSpeed = 10f;
+    [SerializeField] private bool moveHorizontally = true;
+    private Rigidbody2D rb;
+    private Transform collider;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        collider = transform.GetChild(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RotateGlave();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveGlave();
+    }
+
+    private void MoveGlave()
+    {
+        if (moveHorizontally)
+        {
+            //rb.AddForce(new Vector2(movementSpeed, 0));
+            rb.velocity = new Vector2(movementSpeed, 0);
+        }
+        else
+        {
+            //rb.AddForce(new Vector2(0, movementSpeed));
+            rb.velocity = new Vector2(0, movementSpeed);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            collider.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            movementSpeed = -movementSpeed;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            collider.gameObject.SetActive(false);
+        }
+    }
+
+    private void RotateGlave()
+    {
+        transform.Rotate(0, 0, 6.0f * rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ PSMG17/Assets/Scripts/RoomScriptsNew/Room03.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room03 : MonoBehaviour {
+
+    [SerializeField]
+    private Transform player1;
+    [SerializeField]
+    private Transform player2;
+    private Bounds roomBounds;
+
+    private bool playerInside = false;
+    private GameObject glaves;
+
+    void Start () {
+        roomBounds = transform.GetComponent<BoxCollider2D>().bounds;
+        glaves = transform.FindChild("Glaves").gameObject;
+    }
+	
+	void Update () {
+        ActivateGlaves();
+        DeactivateGlaves();
+	}
+
+    private void DeactivateGlaves()
+    {
+        if (!roomBounds.Contains(player1.position) && !roomBounds.Contains(player2.position))
+        {
+            if (playerInside)
+            {
+                playerInside = false;
+                glaves.SetActive(false);
+            }
+        }
+    }
+
+    private void ActivateGlaves()
+    {
+        if (roomBounds.Contains(player1.position) || roomBounds.Contains(player2.position))
+        {
+            if (!playerInside)
+            {
+                playerInside = true;
+                glaves.SetActive(true);
+            }
+        }
+    }
+}
