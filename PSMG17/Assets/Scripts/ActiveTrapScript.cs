3cb2eb3864ff21e6399377d28f833038c12ad12c
Jannik Bikowski <janbiko@gmx.de>
2017-08-29 12:47:44 +0200

    Implemented trap mechanism for trap room

---
+++ PSMG17/Assets/Scripts/ActiveTrapScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveTrapScript : MonoBehaviour {
+
+    private float maxActiveTime = 1f;
+    private float activeTime;
+
+	private void OnEnable()
+    {
+        activeTime = maxActiveTime;
+    }
+	
+	void Update ()
+    {
+        activeTime -= Time.deltaTime;
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && activeTime >= 0)
+        {
+            collision.gameObject.GetComponent<HealthbarController>().currentHealth -= 10;
+        }
+        else if (collision.gameObject.tag == "Enemy" && activeTime >= 0)
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ PSMG17/Assets/Scripts/TrapMechanism.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapMechanism : MonoBehaviour {
+
+    private List<Transform> pressureplates = new List<Transform>();
+    private List<Transform> traps = new List<Transform>();
+
+	// Use this for initialization
+	void Start ()
+    {
+        FillPressureplatesList();
+        FillTrapsList();
+	}
+
+    private void FillPressureplatesList()
+    {
+        for (int i = 0; i < transform.FindChild("PressurePlates").childCount; i++)
+        {
+            pressureplates.Add(transform.FindChild("PressurePlates").GetChild(i));
+        }
+    }
+
+    private void FillTrapsList()
+    {
+        for (int i = 0; i < transform.FindChild("Traps").childCount; i++)
+        {
+            traps.Add(transform.FindChild("Traps").GetChild(i));
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        ActivateTraps();
+	}
+
+    private void ActivateTraps()
+    {
+        for (int i = 0; i < pressureplates.Count; i++)
+        {
+            if (pressureplates[i].GetChild(1).gameObject.activeSelf)
+            {
+                traps[i].GetChild(1).gameObject.SetActive(true);
+            }
+            else
+            {
+                traps[i].GetChild(1).gameObject.SetActive(false);
+            }
+        }
+    }
+}
