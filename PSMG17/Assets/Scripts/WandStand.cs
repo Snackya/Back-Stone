889f4c9df7884dc4b4aa22180df7c9803abdbc1a
Jannik Bikowski <janbiko@gmx.de>
2017-08-30 15:25:31 +0200

    Implemented heal icons & icon switch

--- PSMG17/Assets/Scripts/WandStand.cs
+++ PSMG17/Assets/Scripts/WandStand.cs
@@ -1,14 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WandStand : MonoBehaviour {
+
+    private int newMaxHealth = 100;
+    [SerializeField]
+    private Transform[] skillIcons;
+    [SerializeField]
+    private Sprite player1Icon;
+    [SerializeField]
+    private Sprite player2Icon;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            int playerNumber = collision.gameObject.GetComponent<PlayerController>().playerNumber;
             collision.gameObject.GetComponent<PlayerController>().swordEquipped = false;
+            collision.gameObject.GetComponent<HealthbarController>().maxHealth = newMaxHealth;
+            collision.gameObject.GetComponent<HealthbarController>().currentHealth =
+                collision.gameObject.GetComponent<HealthbarController>().maxHealth;
+            ChangeSkillIcon(playerNumber);
+        }
+    }
+
+    private void ChangeSkillIcon(int pNum)
+    {
+        if (pNum == 1)
+        {
+            skillIcons[0].GetComponent<Image>().sprite = player1Icon;
+            skillIcons[0].GetChild(0).GetComponent<Image>().sprite = player1Icon;
+        }
+        else if (pNum == 2)
+        {
+            skillIcons[1].GetComponent<Image>().sprite = player2Icon;
+            skillIcons[1].GetChild(0).GetComponent<Image>().sprite = player2Icon;
         }
     }
 }
