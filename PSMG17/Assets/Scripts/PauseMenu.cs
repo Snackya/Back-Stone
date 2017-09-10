6bcf8408d0ca59e9444151d380cc2cab41e6b8cc
Snackya <thomas.fischeer@gmail.com>
2017-09-10 22:01:22 +0200

    add pause screen, improve menu navigation

---
+++ PSMG17/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+    public GameObject pauseMenu;
+	
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            HandlePause();
+        }
+    }
+
+	public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void HandlePause()
+    {
+        if (!pauseMenu.activeSelf)
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+}
